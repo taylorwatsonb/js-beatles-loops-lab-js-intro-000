281ee1ce61b3f122838231b36b8ce14dfcd479a9
Taylor Watson <taylorwatsonb@gmail.com>
2019-06-15 14:37:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // add solution here
+function theBeatlesPlay(['Bob Marley', 'Common', 'Marvin Gaye'], ['saxophone', 'guitar', 'flute']){
+  let music = [];
+  for(let i = 0; i < theBeatlesPlay.length; i++)
+  
+}
